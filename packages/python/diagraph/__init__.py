f8ad4a9390d7d85a240ef2022f125eaec102af61
Kevin Scott <151596+thekevinscott@users.noreply.github.com>
2023-10-29 17:02:16 -0400

    Fix imports

--- packages/python/diagraph/__init__.py
+++ packages/python/diagraph/__init__.py
@@ -1,4 +1,4 @@
-from .diagraph import Diagraph as Diagraph
-from .prompt import prompt as prompt
+from .classes.diagraph import Diagraph as Diagraph
+from .decorators.prompt import prompt as prompt
 from .llm.openai_llm import OpenAI as OpenAI
 from .utils.depends import Depends as Depends
