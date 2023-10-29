737a901f73ec0d35252aa0b09066f8effc048f95
Kevin Scott <151596+thekevinscott@users.noreply.github.com>
2023-10-29 11:10:26 -0400

    Fix linting

--- packages/python/diagraph/classes/diagraph.py
+++ packages/python/diagraph/classes/diagraph.py
@@ -1,5 +1,5 @@
 from __future__ import annotations
-from typing import Callable, Generic, Optional, TypeVar, overload
+from typing import Callable, Optional, overload
 from bidict import bidict
 
 from ..utils.annotations import get_dependency, is_annotated
