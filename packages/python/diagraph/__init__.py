22d1e817e6180340452e53a1745f6a5d144ab5ee
Kevin Scott <151596+thekevinscott@users.noreply.github.com>
2023-10-29 17:03:39 -0400

    Fix top level export (#19)
    #17 implemented this, but `ruff` removed them as I wrote them
    incorrectly. This should bring them back.

---
+++ packages/python/diagraph/__init__.py
@@ -0,0 +1,4 @@
+from .classes.diagraph import Diagraph as Diagraph
+from .decorators.prompt import prompt as prompt
+from .llm.openai_llm import OpenAI as OpenAI
+from .utils.depends import Depends as Depends
